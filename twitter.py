073fbae552184c7aff7d674330110839f410a6c1
Unknown <vsriniv@iu.edu>
2018-04-15 21:41:31 -0400

    some change need to check

--- twitter.py
+++ twitter.py
@@ -1,9 +1,22 @@
 import dao
+import pickle as pkl
+import os
 
 
 tweetsDAO = dao.DAO("tweets")
 tweetsDAO.dbConnTest()
 
 
-def persistFromPickle(fileName):
-	pass
+def persistFromPickle():
+	Months = ["October", "November", "December", "January", "February", "March"]
+	DataFolderPath = "metoo_data/"
+	tweets = []
+	for month in Months:
+		for file in os.listdir(DataFolderPath + month):
+			if file.endswith(".pkl"):
+				tweets.extend(pkl.load(open(DataFolderPath + month + "/" + file, "rb")))
+
+	tweetsDAO.batchCreate(tweets)
+
+
+persistFromPickle()
